144e9c4b47b9d6b37ba5f7b6923e2b81c20e6cb2
Asafe Mota <contatoasapdev@gmail.com>
2025-06-15 22:17:28 -0300

    segundosss

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }));
 
-mongoose.connect('mongodb://localhost:27017/blogforum', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
